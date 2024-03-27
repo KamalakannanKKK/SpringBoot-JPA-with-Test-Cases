0bf4fa5fdf17818ccb1ce41b001d0fdd99bf363e
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-03-27 17:51:51 +0530

    Create SpringBootJpAwithTestCasesApplication.java

---
+++ src/main/java/com/spring/SpringBootJPAwithTestCases/SpringBootJpAwithTestCasesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.SpringBootJPAwithTestCases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpAwithTestCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpAwithTestCasesApplication.class, args);
+	}
+
+}
