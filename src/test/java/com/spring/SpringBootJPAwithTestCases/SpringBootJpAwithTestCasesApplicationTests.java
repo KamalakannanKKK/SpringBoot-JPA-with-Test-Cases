793592f7cb2d662ce2db4d6e4c52a751a71b504c
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-03-27 17:56:58 +0530

    Create SpringBootJpAwithTestCasesApplicationTests.java

---
+++ src/test/java/com/spring/SpringBootJPAwithTestCases/SpringBootJpAwithTestCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringBootJPAwithTestCases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpAwithTestCasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
